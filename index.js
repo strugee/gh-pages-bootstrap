c6c91849133af5c2ad45104b27288c2f4d4b4195
Alex Jordan <alex@strugee.net>
2015-08-10 22:20:26 -0400

    Use gh-search-branch

--- index.js
+++ index.js
@@ -1,49 +1,17 @@
 var https = require('https');
 var spawn = require('child_process').spawn;
+var search_branch = require('gh-search-branch');
 
 module.exports = function(user, auth_token, callback) {
-	https.get({hostname: 'api.github.com', path: '/users/' + user + '/repos', headers: {'User-Agent': 'gh-pages-bootstrap/1.0.0', 'Authorization': 'token ' + auth_token}}, function(res) {
-		var repos;
-		var reposRaw = '';
-		
-		res.on('data', function(chunk) {
-			reposRaw += chunk.toString();
+	var search_options = {
+		'ua': 'gh-pages-bootstrap/1.1.0',
+		'token': auth_token
+	};
+	search_branch(user, 'gh-pages', search_options, function(err, repo) {
+		if (err) callback(err);
+		spawn('git', ['clone', '--branch', 'gh-pages', 'git://github.com/' + user + '/' + repo]).on('error', function(err) {
+			callback(err);
 		});
-
-		res.on('end', function() {
-			repos = JSON.parse(reposRaw);
-			
-			repos.forEach(function(repo) {
-				https.get({
-					hostname: 'api.github.com',
-					path: '/repos/' + user + '/' + repo.name + '/branches',
-					headers: {'User-Agent': 'gh-pages-bootstrap/1.0.0', Authorization: 'token ' + auth_token}
-				}, function(res) {
-					var branches;
-					var branchesRaw = '';
-					
-					res.on('data', function(chunk) {
-						branchesRaw += chunk.toString();
-					});
-					
-					res.on('end', function() {
-						branches = JSON.parse(branchesRaw);
-						for (var j in branches) {
-							var branch = branches[j];
-							if (branch.name === 'gh-pages') {
-								spawn('git', ['clone', '--branch', 'gh-pages', 'git://github.com/' + user + '/' + repo.name]).on('error', function(err) {
-									callback(err);
-								});
-								console.log('Spawned git process for ' + repo.name);
-							}
-						}
-					});
-				}).on('error', function(err) {
-					callback(err);
-				});
-			});
-		});
-	}).on('error', function(err) {
-		callback(err);
+		console.log('Spawned git process for ' + repo);
 	});
 };
