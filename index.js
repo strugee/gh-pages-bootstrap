18e051073727058347502d4920c7a5405d620f1f
AJ Jordan <alex@strugee.net>
2019-03-01 02:33:14 -0500

    Use git-clone-or-pull
    Fixes #1

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var spawn = require('child_process').spawn;
 var search_branch = require('gh-search-branch');
+var cloneOrPull = require('git-clone-or-pull');
 var pkg = require('./package.json');
 
 module.exports = function(user, auth_token, callback) {
@@ -9,8 +9,12 @@ module.exports = function(user, auth_token, callback) {
 	};
 	search_branch(user, 'gh-pages', search_options, function(err, repo) {
 		if (err) callback(err);
-		spawn('git', ['clone', '--branch', 'gh-pages', 'git://github.com/' + user + '/' + repo]).on('error', function(err) {
-			callback(err);
+		cloneOrPull('git://github.com/' + user + '/' + repo, {
+			branch: 'gh-pages',
+			path: '.'
+		}, repo, function(err) {
+			// Compat: 1.x only calls the callback on failures
+			if (err) callback(err);
 		});
 		console.log('Spawned git process for ' + repo);
 	});
