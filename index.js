af03e9268122de2691975d65fda2f1fce7210be8
AJ Jordan <alex@strugee.net>
2019-03-01 02:30:26 -0500

    Make the User-Agent use package.json's version

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var https = require('https');
 var spawn = require('child_process').spawn;
 var search_branch = require('gh-search-branch');
+var pkg = require('./package.json');
 
 module.exports = function(user, auth_token, callback) {
 	var search_options = {
-		'ua': 'gh-pages-bootstrap/1.1.0',
+		'ua': 'gh-pages-bootstrap/' + pkg.version,
 		'token': auth_token
 	};
 	search_branch(user, 'gh-pages', search_options, function(err, repo) {
